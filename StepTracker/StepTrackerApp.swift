62bb9f07c49cd0a3697253642938a2cd983ded76
Shilan <158176141+sh-raz@users.noreply.github.com>
2026-08-26 16:56:33 +0100

    Initial commit

---
+++ StepTracker/StepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTrackerApp.swift
+//  StepTracker
+//
+//  Created by Shilan on 26/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct StepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
