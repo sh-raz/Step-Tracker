69ab54d9ee4928c419f75128021debad5a1ebc43
Shilan <158176141+sh-raz@users.noreply.github.com>
2026-08-27 10:19:43 +0100

    Create dashboard UI

--- StepTracker/ContentView.swift
+++ StepTracker/ContentView.swift
@@ -9,13 +9,60 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            ScrollView{
+                VStack(spacing: 20){
+                    
+                    VStack {
+                        HStack {
+                            VStack(alignment: .leading){
+                                Label("Steps", systemImage: "figure.walk")
+                                    .font(.title3.bold())
+                                    .foregroundStyle(Color.pink)
+                                Text("Avg: 10K Steps")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
+                            Spacer()
+                            Image(systemName: "chevron.forward")
+                                .foregroundStyle(.secondary)
+                        }
+                        .padding(.bottom, 12)
+    
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(.secondary)
+                            .frame(height: 150)
+                    }
+                    .padding()
+                    .background {
+                        RoundedRectangle(cornerRadius: 15)
+                        .fill(Color(.secondarySystemBackground))                    }
+                    
+                    
+                    VStack(alignment: .leading){
+                        VStack(alignment: .leading){
+                                Label("Averages", systemImage: "calendar")
+                                    .font(.title3.bold())
+                                    .foregroundStyle(Color.pink)
+                                Text("Last 28 days")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
+                        .padding(.bottom, 12)
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(.secondary)
+                            .frame(height: 240)
+                    }
+                    .padding()
+                    .background {
+                        RoundedRectangle(cornerRadius: 15)
+                        .fill(Color(.secondarySystemBackground))
+                    }
+                }
+            }
+            .padding()
+            .navigationTitle("Dashboard")
         }
-        .padding()
     }
 }
 
